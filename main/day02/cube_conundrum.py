71e576836cef2fec8c7dc2159780201454da73c4
Ben Watson <benwatson528@gmail.com>
2023-12-02 07:35:17 +0000

    Day 2 pt 2

--- main/day02/cube_conundrum.py
+++ main/day02/cube_conundrum.py
@@ -1,6 +1,21 @@
-def solve(games, max_cubes) -> int:
+import math
+
+
+def solve_p1(games, max_cubes) -> int:
     return sum([i + 1 for i, game in enumerate(games) if play_round(game, max_cubes)])
+
+
+def solve_p2(games) -> int:
+    return sum(play_max_round(game) for game in games)
 
 
 def play_round(game, max_cubes):
     return all(all(num_in_bag <= max_cubes[colour] for colour, num_in_bag in subset.items()) for subset in game)
+
+
+def play_max_round(game):
+    max_vals = {"red": 0, "green": 0, "blue": 0}
+    for subset in game:
+        for colour, num_in_bag in subset.items():
+            max_vals[colour] = max(num_in_bag, max_vals[colour])
+    return math.prod(max_vals.values())
--- tests/day02/test_cube_conundrum.py
+++ tests/day02/test_cube_conundrum.py
@@ -1,15 +1,23 @@
 import os
 from pathlib import Path
 
-from main.day02.cube_conundrum import solve
+from main.day02.cube_conundrum import solve_p1, solve_p2
 
 
 def test_p1_simple():
-    assert solve(read_input("data/test_input.txt"), {"red": 12, "green": 13, "blue": 14}) == 8
+    assert solve_p1(read_input("data/test_input.txt"), {"red": 12, "green": 13, "blue": 14}) == 8
 
 
 def test_p1_real():
-    assert solve(read_input("data/input.txt"), {"red": 12, "green": 13, "blue": 14}) == 2164
+    assert solve_p1(read_input("data/input.txt"), {"red": 12, "green": 13, "blue": 14}) == 2164
+
+
+def test_p2_simple():
+    assert solve_p2(read_input("data/test_input.txt")) == 2286
+
+
+def test_p2_real():
+    assert solve_p2(read_input("data/input.txt")) == 69929
 
 
 def read_input(file_name):
