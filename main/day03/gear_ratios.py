746b5cae03f5e9657b51c31858339b1b2efe9a12
Ben Watson <benwatson528@gmail.com>
2023-12-03 09:55:00 +0000

    Day 3 pt 2

--- main/day03/gear_ratios.py
+++ main/day03/gear_ratios.py
@@ -1,10 +1,29 @@
+import math
 from itertools import product
 
 ADJACENT_CELLS = set(product([-1, 0, 1, -1, 0, 1], repeat=2))
 
 
-def solve(numbers, symbols) -> int:
+def solve_p1(numbers, symbols) -> int:
     return sum(int(num_val) for num_coord, num_val in numbers.items() if process_number(num_coord, num_val, symbols))
+
+
+def solve_p2(numbers, symbols) -> int:
+    all_number_coords = {}
+    for num_coord, num_val in numbers.items():
+        for x_range in range(num_coord[0], num_coord[0] + len(num_val)):
+            all_number_coords[(x_range, num_coord[1])] = num_val
+    all_star_coords = {k for k, v in symbols.items() if v == '*'}
+    return sum(find_adjacent_numbers(all_number_coords, star_coord) for star_coord in all_star_coords)
+
+
+def find_adjacent_numbers(all_number_coords, star_coord):
+    adjacent_nums = set()
+    for adj in ADJACENT_CELLS:
+        adjacent_cell = star_coord[0] + adj[0], star_coord[1] + adj[1]
+        if adjacent_cell in all_number_coords.keys():
+            adjacent_nums.add(int(all_number_coords[adjacent_cell]))
+    return math.prod(adjacent_nums) if len(adjacent_nums) == 2 else 0
 
 
 def process_number(num_coord, num_val, symbols):
@@ -12,4 +31,4 @@ def process_number(num_coord, num_val, symbols):
 
 
 def is_adjacent_symbol(coord, symbols):
-    return len(symbols.intersection([(coord[0] + adj[0], coord[1] + adj[1]) for adj in ADJACENT_CELLS])) > 0
+    return len(set(symbols.keys()).intersection([(coord[0] + adj[0], coord[1] + adj[1]) for adj in ADJACENT_CELLS])) > 0
--- tests/day03/test_gear_ratios.py
+++ tests/day03/test_gear_ratios.py
@@ -2,27 +2,37 @@ import os
 import re
 from pathlib import Path
 
-from main.day03.gear_ratios import solve
+from main.day03.gear_ratios import solve_p1, solve_p2
 
 
 def test_p1_simple():
     numbers, symbols = read_input("data/test_input.txt")
-    assert solve(numbers, symbols) == 4361
+    assert solve_p1(numbers, symbols) == 4361
 
 
 def test_p1_real():
     numbers, symbols = read_input("data/input.txt")
-    assert solve(numbers, symbols) == 535351
+    assert solve_p1(numbers, symbols) == 535351
+
+
+def test_p2_simple():
+    numbers, symbols = read_input("data/test_input.txt")
+    assert solve_p2(numbers, symbols) == 467835
+
+
+def test_p2_real():
+    numbers, symbols = read_input("data/input.txt")
+    assert solve_p2(numbers, symbols) == 87287096
 
 
 def read_input(file_name):
     with open(os.path.join(Path(__file__).parent.absolute(), file_name)) as f:
         numbers = {}
-        symbols = set()
+        symbols = {}
         for j, row in enumerate(f.read().splitlines()):
             for m in re.finditer(r'\d+', row):
                 numbers[(m.start(), j)] = m.group()
             for i, c in enumerate(row):
                 if not c.isalnum() and c != '.':
-                    symbols.add((i, j))
+                    symbols[(i, j)] = c
     return numbers, symbols
