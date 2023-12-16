f54228c428048d5eede96c4356247288cde5f101
Ben Watson <benwatson528@gmail.com>
2023-12-16 09:58:47 +0000

    Day 16 part 2

--- main/day16/the_floor_will_be_lava.py
+++ main/day16/the_floor_will_be_lava.py
@@ -1,10 +1,18 @@
 from enum import Enum
 
 
-def solve(tiles) -> int:
+def solve_p1(tiles) -> int:
     start = ((0, 0), Direction.R)
-    visited = fire_beam(start, tiles)
-    return len({coord for coord, direction in visited})
+    return len(fire_beam(start, tiles))
+
+
+def solve_p2(tiles) -> int:
+    max_x, max_y = max(x for x, y in tiles), max(y for x, y in tiles)
+    left_entries = [((0, y), Direction.R) for y in range(max_y + 1)]
+    right_entries = [((max_x, y), Direction.L) for y in range(max_y + 1)]
+    top_entries = [((x, 0), Direction.D) for x in range(max_x + 1)]
+    bottom_entries = [((x, max_y), Direction.U) for x in range(max_x + 1)]
+    return max(len(fire_beam(start, tiles)) for start in left_entries + right_entries + top_entries + bottom_entries)
 
 
 def fire_beam(start, tiles):
@@ -18,7 +26,7 @@ def fire_beam(start, tiles):
         for new_coord, new_direction in move(coord, direction, tiles):
             if new_coord in tiles.keys() and (new_coord, new_direction) not in visited:
                 to_visit.append((new_coord, new_direction))
-    return visited
+    return {coord for coord, direction in visited}
 
 
 def move(coord, direction, tiles):
@@ -26,43 +34,33 @@ def move(coord, direction, tiles):
         case '.':
             return [step_coord(coord, direction)]
         case '/':
-            return [traverse_left_splitter(coord, direction)]
+            return [traverse_diagonal_splitter(coord, direction, is_reversed=True)]
         case '\\':
-            return [traverse_right_splitter(coord, direction)]
+            return [traverse_diagonal_splitter(coord, direction, is_reversed=False)]
         case '-':
-            if direction in (Direction.L, Direction.R):
-                return [step_coord(coord, direction)]
-            else:
-                return [step_coord(coord, Direction.L), step_coord(coord, Direction.R)]
+            return traverse_straight_splitter(coord, direction, is_horizontal=True)
         case '|':
-            if direction in (Direction.U, Direction.D):
-                return [step_coord(coord, direction)]
-            else:
-                return [step_coord(coord, Direction.U), step_coord(coord, Direction.D)]
+            return traverse_straight_splitter(coord, direction, is_horizontal=False)
 
 
-def traverse_left_splitter(coord, direction):  # /
+def traverse_diagonal_splitter(coord, direction, is_reversed):
     match direction:
         case Direction.R:
-            return step_coord(coord, Direction.U)
+            return step_coord(coord, Direction.U if is_reversed else Direction.D)
         case Direction.L:
-            return step_coord(coord, Direction.D)
+            return step_coord(coord, Direction.D if is_reversed else Direction.U)
         case Direction.U:
-            return step_coord(coord, Direction.R)
+            return step_coord(coord, Direction.R if is_reversed else Direction.L)
         case Direction.D:
-            return step_coord(coord, Direction.L)
+            return step_coord(coord, Direction.L if is_reversed else Direction.R)
 
 
-def traverse_right_splitter(coord, direction):  # \
-    match direction:
-        case Direction.R:
-            return step_coord(coord, Direction.D)
-        case Direction.L:
-            return step_coord(coord, Direction.U)
-        case Direction.U:
-            return step_coord(coord, Direction.L)
-        case Direction.D:
-            return step_coord(coord, Direction.R)
+def traverse_straight_splitter(coord, direction, is_horizontal):
+    pass_through_directions = (Direction.L, Direction.R) if is_horizontal else (Direction.U, Direction.D)
+    if direction in pass_through_directions:
+        return [step_coord(coord, direction)]
+    else:
+        return [step_coord(coord, new_direction) for new_direction in pass_through_directions]
 
 
 def step_coord(coord, direction):
